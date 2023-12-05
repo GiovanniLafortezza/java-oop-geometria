b047a29e998f719a4e42ca3a2f424d159a0d7909
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-05 14:12:35 +0100

    create constructor and methods

--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -11,4 +11,16 @@ public class Rettangolo {
         this.base = base;
         this.height = height;
     }
+
+    //METODI
+
+    int perimetro() {
+        int valuePerimetro = (base * 2) + (height * 2);
+        return valuePerimetro;
+    }
+
+    int area() {
+        int valueArea = (base * height) / 2;
+        return valueArea;
+    }
 }
