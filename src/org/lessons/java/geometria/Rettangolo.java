63d5920bbf196030d54cd9eb6675bef25cbc64c7
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-05 13:58:38 +0100

    create class Rettangolo

---
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -0,0 +1,14 @@
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+    //ATTRIBUTI
+    int base;
+    int height;
+
+    //COSTRUTTORI
+
+    public Rettangolo(int base, int height) {
+        this.base = base;
+        this.height = height;
+    }
+}
